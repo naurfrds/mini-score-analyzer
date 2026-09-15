654888d89670564a01b46e08957b4482b9ff67a4
Naura Faradisa <naurfrds@gmail.com>
2026-09-15 17:51:28 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,45 @@
-
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+int score, total = 0;
+int passed = 0;
+int highest;
+vector<int> scores;
+// Nilai 1
+cin >> score;
+scores.push_back(score);
+total += score;
+if (score >= 60) passed++;
+highest = score;
+// Nilai 2
+cin >> score;
+scores.push_back(score);
+total += score;
+if (score >= 60) passed++;
+if (score > highest) highest = score;
+// Nilai 3
+cin >> score;
+scores.push_back(score);
+total += score;
+if (score >= 60) passed++;
+if (score > highest) highest = score;
+// Nilai 4
+cin >> score;
+scores.push_back(score);
+total += score;
+if (score >= 60) passed++;
+if (score > highest) highest = score;
+// Nilai 5
+cin >> score;
+scores.push_back(score);
+total += score;
+if (score >= 60) passed++;
+if (score > highest) highest = score;
+double average = (double) total / scores.size();
+cout << "Total : " << total << endl;
+cout << "Rata-rata : " << average << endl;
+cout << "Tertinggi : " << highest << endl;
+cout << "Lulus : " << passed << endl;
+return 0;
+}
